e5e2ce5a0016dd3624a106201eed42f1f38af92a
Thanatoslayer6 <Thanatoslayer6@gmail.com>
2024-07-30 14:05:02 +0800

    Savin

--- linked_list.c
+++ linked_list.c
@@ -1,38 +1,67 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct {
+typedef struct Node
+{
     int data;
     struct Node *next;
 } Node;
 
-
 // Add a node to the end of the list
-void add(Node *head, int data) {
-    Node *new_node = malloc(sizeof(Node));
-    new_node->data = data;
-    if (head == NULL) { 
-        head = new_node;
-        new_node->next = NULL;
-    } else {
-        new_node->next = head;
+void add(Node *head, int data)
+{
+    Node *item = malloc(sizeof(Node));
+    item->data = data;
+    item->next = NULL;
+    if (head == NULL)
+    { // Means the list is empty
+        head = item;
+    }
+    else
+    { // Traverse at the end of the list
+        Node *iterator = head;
+        while (iterator->next != NULL)
+        {
+            iterator = iterator->next;
+        }
+        iterator = item;
     }
 }
 
-void printList(Node *head) {
+void printList(Node *head)
+{
     Node *iterator = head;
-    while (iterator->next != NULL) {
+    while (iterator != NULL)
+    {
         printf("node value: %d\n", iterator->data);
         iterator = iterator->next;
     }
 }
 
+// Create head
 
-int main() {
-    // Create head
+int main()
+{
     Node *head = NULL;
-    add(head, 5);
     add(head, 2);
+    printf("Hello");
+    // add(head, 5);
+    // add(head, 2);
     // Traverse the linked list
-    printList(head);
+    // printList(head);
+
+    /*
+    Node *item1 = malloc(sizeof(Node));
+    item1->data = 5;
+    item1->next = NULL;
+    head = item1;
+
+    Node *item2 = malloc(sizeof(Node));
+    item2->data = 2;
+    item2->next = NULL;
+    item1->next = item2;
+
+    free(item1);
+    free(item2);
+    */
 }
