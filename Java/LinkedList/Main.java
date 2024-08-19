cae931b32eb5e78fafb87c98f84ff59ede60e6d9
Thanatoslayer6 <Thanatoslayer6@gmail.com>
2024-08-19 19:22:44 +0800

    Saving progress

---
+++ Java/LinkedList/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String args[]) {
+        Node item_1 = new Node(2);
+        Node item_2 = new Node(4);        
+        Node item_3 = new Node(6);
+    }
+}
