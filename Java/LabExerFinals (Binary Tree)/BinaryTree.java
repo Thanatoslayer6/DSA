8d65164afa28e6bbd32962e613d5f62cc8a80a4d
Thanatoslayer6 <Thanatoslayer6@gmail.com>
2024-12-02 14:55:58 +0800

    Working on Binary Tree

--- Java/LabExerFinals (Binary Tree)/BinaryTree.java
+++ Java/LabExerFinals (Binary Tree)/BinaryTree.java
@@ -14,60 +14,207 @@ class Node {
 
 public class BinaryTree {
     public static Node root;
-    int level, edges;
+    int level, edges, nodeCount;
     public BinaryTree(int key) {
         this.root = new Node(key);
-        this.level = 1;
-        this.edges = this.level - 1; // n - 1
+        this.nodeCount = 1;
+        this.level = (int) Math.floor(Math.log(nodeCount) / Math.log(2)) + 1;
+        this.edges = this.nodeCount - 1; // n - 1
     }
-
+ 
     public void insert(int key) {
-        // Decide where to place the node, starting from the root node
-        Node temp = this.root;
-        while (true) {
-            if (key >= temp.key) { // Insert to right
-                if (temp.right == null) {
-                    temp.right = new Node(key);
-                    break;
-                }
-                temp = temp.right;
-                continue;
+      
+        // If the tree is empty, simply do nothing and return
+        if (this.root == null) {
+            return;
+        }
+
+        // We perform level order traversal (BFS) until we find an empty place
+        // either left or right child of some node pointing to null
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(this.root);
+
+        while (!queue.isEmpty()) { 
+            Node node = queue.poll(); // Queue dequeues
+
+            // First check left if left is null insert node in left otherwise check for right
+            if (node.left != null)
+                queue.add(node.left);
+            else {
+                node.left = new Node(key);
+                break;
             }
-            if (key <= root.key) { // Insert to left
-                if (temp.left == null) {
-                    temp.left = new Node(key);
-                    break;
-                }
-                temp = temp.left;
-                continue;
+
+            if (node.right != null)
+                queue.add(node.right);
+            else {
+                node.right = new Node(key);
+                break;
             }
         }
 
-        this.level++;
-        this.edges = this.level - 1;
+        this.nodeCount++;
+        this.level = (int) Math.floor(Math.log(nodeCount) / Math.log(2)) + 1;
+        this.edges = this.nodeCount - 1; // n - 1
     }
-    
+ 
     public void delete(int key) {
-                
+        if (this.root == null)
+            return;
+
+        // If the tree has only one node
+        if (this.nodeCount == 1) { 
+            // If the root node is the key, delete it
+            if (this.root.key == key) {
+                this.root = null;
+                this.nodeCount = 0;
+                this.level = 0;
+                this.edges = 0;
+            }
+            return;
+        }
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(this.root);
+
+        Node current = null;
+        Node keyNode = null;
+
+        // Perform level order traversal to find the deepest node and the key node
+        while (!queue.isEmpty()) {
+            current = queue.poll();
+
+            // If current node is the key node
+            if (current.key == key)
+                keyNode = current;
+
+            if (current.left != null)
+                queue.add(current.left);
+
+            if (current.right != null)
+                queue.add(current.right);
+        }
+
+        // If key node is found, replace its
+        // data with the deepest node
+        if (keyNode != null) {
+          
+            // Store the data of the
+            // deepest node
+            int x = current.key; 
+          
+            // Replace key node data with 
+            // deepest node's data
+            keyNode.key = x; 
+          
+            // Delete the deepest node
+            deleteDeepestNode(current);
+        }
+
     }
 
+    private void deleteDeepestNode(Node dNode) {
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(this.root);
+
+        Node current;
+        while (!queue.isEmpty()) {
+            current = queue.poll();
+
+            // If current node is the deepest node, delete it
+            if (current == dNode) {
+                current = null;
+                dNode = null;
+                return;
+            }
+
+            // Check the right child first
+            if (current.right != null) {
+              
+                // If right child is the deepest node,
+                  // delete it
+                if (current.right == dNode) {
+                    current.right = null;
+                    dNode = null;
+                    return;
+                }
+                queue.add(current.right);
+            }
+
+            // Check the left child
+            if (current.left != null) {
+              
+                // If left child is the deepest node,
+                  // delete it
+                if (current.left == dNode) {
+                    current.left = null;
+                    dNode = null;
+                    return;
+                }
+                queue.add(current.left);
+            }
+        }
+    }
+
+
+    public String preorder() {
+        return preorder_recursive(this.root, "");
+    }
+
+    public String inorder() {
+        return inorder_recursive(this.root, "");
+    }
+
+    public String postorder() {
+        return postorder_recursive(this.root, "");
+    }
+
+    private String preorder_recursive(Node current, String res) {
+        if (current == null) {
+            return res;
+        }
+        res += current.key + " ";
+        res = preorder_recursive(current.left, res);
+        res = preorder_recursive(current.right, res);
+        return res;
+    }
+
+    private String inorder_recursive(Node current, String res) {
+        if (current == null) {
+            return res;
+        }
+        res = inorder_recursive(current.left, res);
+        res += current.key + " ";
+        res = inorder_recursive(current.right, res);
+        return res;
+    }
+
+    private String postorder_recursive(Node current, String res) {
+        if (current == null) {
+            return res;
+        }
+        res = postorder_recursive(current.left, res);
+        res = postorder_recursive(current.right, res);
+        res += current.key + " ";
+        return res;
+    }
     
-    public void printBFS() {
+    public String bfs() {
         // Initialize queue, then add the root node
         Queue<Node> queue = new LinkedList<>();
         queue.add(this.root);
-        // Initialize a list to store the traversal sequence
+        // Initialize a string to store the traversal sequence
         // when a node is traversed, it should be added to the queue
-        ArrayList<Integer> list = new ArrayList<>();
+        String result = "";
         while (!queue.isEmpty()) {
             Node node = queue.poll(); // Queue dequeues
-            list.add(node.key);           // Save node value
+            result += node.key + " "; // Concatenate node key into string
             if (node.left != null)
                 queue.offer(node.left);   // Left child node enqueues
             if (node.right != null)
                 queue.offer(node.right);  // Right child node enqueues
         }
-        System.out.println(list.toString());
+        return result;
     }
 
 }
--- Java/LabExerFinals (Binary Tree)/Main.java
+++ Java/LabExerFinals (Binary Tree)/Main.java
@@ -1,13 +1,17 @@
 public class Main {
     public static void main(String args[]) {
-        BinaryTree tree = new BinaryTree(5);
-        tree.insert(4);
-        tree.insert(1);
-        tree.insert(6);
-        tree.insert(8);
+        BinaryTree tree = new BinaryTree(10);
+        tree.insert(11);
+        tree.insert(9);
+        tree.insert(7);
         tree.insert(12);
-        tree.insert(2);
-        tree.print();
-
+        tree.insert(15);
+        tree.insert(8);
+        
+        tree.delete(11);
+        // System.out.println(tree.preorder());
+        System.out.println(tree.inorder());
+        // System.out.println(tree.postorder());
+        System.out.println(tree.bfs());
     }
 }
